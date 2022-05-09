04c568cced72f08b127b01ff389adb14aa11ddc1
f3rreros <deabajrami1@gmail.com>
2022-05-10 00:31:26 +0200

    simple calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+
+const add = function(a, b) {
+  return (a + b);
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+  return (a-b);
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let total = 0;
+  for (let i = 0; i < array.length; i++){
+    total += array[i];
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let product = 1;
+  for (let i = 0; i < array.length; i++){
+    product *= array[i];
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return (a ** b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let answer = 1;
+  for(let i = a ; i > 0 ; i-- ){
+    answer *= i;
+      }
+  
+  return answer;
 };
 
 // Do not edit below this line
