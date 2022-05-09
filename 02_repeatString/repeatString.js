d4e58ef211c5a9f42de5e84b94068dbd5b70f5f0
f3rreros <deabajrami1@gmail.com>
2022-05-09 14:55:25 +0200

    Function used for concatinating words Nth-number of times.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
+const repeatString = function(string,num) {
+    let str = string.toString();
+    if (num === 0) {
+        return ""
+    } else if (num < 0) {
+        return "ERROR"
+    }else {
+    for (let i = 0; i < (num - 1); i++){
+        str = string.concat(str)
+     }
+     return str}
+    
 
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
