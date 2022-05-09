75c9717db7c34c5fbd7bd6dc1817fe247cebd8fb
f3rreros <deabajrami1@gmail.com>
2022-05-09 23:42:45 +0200

    basic draft of a sum program

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,20 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let i = a;
+    let sum = 0;
+    if (a < 0 || b < 0){
+        return "ERROR"
+    } else if (typeof a !== "number" || typeof b !== "number"){
+        return "ERROR"
+    } else if (a > b) {
+        for (i ; i > (b-1) ; i-- ){
+            sum += i;
+        } return sum;
+    }
+    else if (a < b){
+        for (i;i < (b+1);i++){
+            sum += i;
+    }
+    return sum;}
 
 };
 
