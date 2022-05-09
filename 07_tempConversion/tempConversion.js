e7ca7976b73ac0b8e5ee2d54efe902a7bcf61a90
f3rreros <deabajrami1@gmail.com>
2022-05-10 00:06:06 +0200

    simple temp conversion using basic math

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(num) {
+  const celc = ((num - 32) * (5/9));
+  return (Math.round(celc * 10) / 10)
+  
 };
 
-const ctof = function() {
-
+const ctof = function(num) {
+  const fahr = ((num * (9/5)) + 32);
+  return (Math.round(fahr * 10) / 10)
+  
 };
 
 // Do not edit below this line
